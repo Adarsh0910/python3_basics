fb742e47dad285411689a9aea5d9aedf28922a42
Adarsh0910 <adarshshresth01@gmail.com>
2019-11-19 08:13:57 +0530

    bucket api changes ^

--- bucketapi/admin.py
+++ bucketapi/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class BucketapiAdmin(admin.ModelAdmin):
+    list_display = ("name",  "date_created", "date_modified")
+
+admin.site.register(models.Bucketlist, BucketapiAdmin)
+#admin.site.register(models.Category, CategoryAdmin)
+
--- bucketapi/models.py
+++ bucketapi/models.py
@@ -1,3 +1,16 @@
+# api/models.py
+
 from django.db import models
 
 # Create your models here.
+
+class Bucketlist(models.Model):
+    """This class represents the bucketlist model."""
+    #id = models.TextField(blank=True)
+    name = models.CharField(max_length=255, blank=False, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return a human readable representation of the model instance."""
+        return "{}".format(self.name)
